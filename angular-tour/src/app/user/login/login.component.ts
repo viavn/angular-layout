3e295be537370e476fbd26e7f7847892c3e2d5bf
Vinicius Avansini <vi.avn@hotmail.com>
2022-09-05 23:19:22 -0300

    adding user selectors

--- angular-tour/src/app/user/login/login.component.ts
+++ angular-tour/src/app/user/login/login.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { State } from 'src/app/state/app.state';
 import { AuthService } from '../auth.service';
+import { getMaskUserName } from '../state/user.reducer';
 
 @Component({
   selector: 'app-login',
@@ -17,16 +19,14 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private store: Store<any>,
+    private store: Store<State>,
   ) { }
 
   ngOnInit(): void {
     // TODO: Unsubscribe
-    this.store.select('users').subscribe(users => {
-      if (users) {
-        this.maskUserName = users.maskUserName;
-      }
-    });
+    this.store.select(getMaskUserName).subscribe(
+      maskUserName => this.maskUserName = maskUserName
+    );
   }
 
   cancel(): void {
--- angular-tour/src/app/user/state/user.reducer.ts
+++ angular-tour/src/app/user/state/user.reducer.ts
@@ -1,8 +1,31 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
+import { User } from "../user";
+
+export interface UserState {
+  currentUser: User | null;
+  maskUserName: boolean;
+}
+
+const initialState: UserState = {
+  currentUser: null,
+  maskUserName: false
+};
+
+const getUserFeatureState = createFeatureSelector<UserState>('users');
+
+export const getMaskUserName = createSelector(
+  getUserFeatureState,
+  state => state.maskUserName
+);
+
+export const getCurrentUser = createSelector(
+  getUserFeatureState,
+  state => state.currentUser
+);
 
 export const userReducer = createReducer(
-  { maskUserName: false },
-  on(createAction('[User] Mask User Name'), (state: any) => {
+  initialState,
+  on(createAction('[User] Mask User Name'), state => {
     return {
       ...state,
       maskUserName: !state.maskUserName
